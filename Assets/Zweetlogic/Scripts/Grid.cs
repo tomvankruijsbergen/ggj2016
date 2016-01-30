17a82ba99f4ed0834e1713c663c0b7152f7f21a0
Robert Mooijman <jambi@Roberts-MacBook-Pro.local>
2016-01-30 21:24:04 +0100

    added zweetlogic

---
+++ Assets/Zweetlogic/Scripts/Grid.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class Grid : MonoBehaviour {
+	
+	protected Dictionary<Vector2, GridItem> objectsByPosition = new Dictionary<Vector2, GridItem>();
+
+	protected PixelMap pixelMap = new PixelMap();
+	public List<Texture> gridSetups = new List<Texture>();
+
+	public List<GameObject> itemTypes = new List<GameObject> ();
+	public Vector3 randomOffsetRadius = new Vector3();
+
+	void Start() {
+		Debug.Log ("converting " + Time.realtimeSinceStartup);
+		int childCount = transform.childCount;
+		for (int i = 0; i < childCount; i++) {
+			Transform row = transform.GetChild (i);
+
+			int cellCount = row.childCount;
+			for (int j = 0; j < cellCount; j++) {
+				Transform cell = row.GetChild (j);
+
+				GridItem cellItem = cell.GetComponent<GridItem> ();
+				cellItem.position = new Vector2 (j, i);
+
+				this.objectsByPosition.Add (cellItem.position, cellItem);
+			}
+		}
+		Debug.Log ("populating " + Time.realtimeSinceStartup);
+		this.populateGrid (false);
+		Debug.Log ("done " + Time.realtimeSinceStartup);
+	}
+
+	private void populateGrid(bool animated) {
+		int randomIndex = Mathf.FloorToInt (Random.Range (0, this.gridSetups.Count));
+		Texture2D texture = this.gridSetups [randomIndex] as Texture2D;
+		Dictionary<Vector2, bool> pixelData = this.pixelMap.pixelmapFromTexture (texture);
+
+		/* Steps:
+		 * For each item,
+		 * - either disable or enable it based on a pixel map.
+		 * - enabled items become a type.
+		 */
+		foreach (var item in this.objectsByPosition) {
+			bool isEnabled = pixelData [item.Key];
+			item.Value.setUsed (isEnabled);
+
+			if (isEnabled == true) {
+				int randomTypeIndex = Random.Range(0, this.itemTypes.Count);
+				GameObject itemType = this.itemTypes [randomTypeIndex];
+
+				Vector3 randomOffset = new Vector3 ();
+				randomOffset.x = Random.Range (-this.randomOffsetRadius.x, this.randomOffsetRadius.x);
+				randomOffset.y = Random.Range (-this.randomOffsetRadius.y, this.randomOffsetRadius.y);
+				randomOffset.z = Random.Range (-this.randomOffsetRadius.z, this.randomOffsetRadius.z);
+				item.Value.changeToType (itemType, animated, randomOffset);
+					
+				GameLogic.ItemStates randomState = (GameLogic.ItemStates)Mathf.FloorToInt (Random.Range (0, (int)GameLogic.ItemStates._All));
+				item.Value.changeToState (randomState, animated);
+			}
+		}
+
+	}
+
+	public static string StringFromPosition(Vector2 position) {
+		return Mathf.Round(position.x) + "-" + Mathf.Round(position.y);
+	}
+	public static Vector2 PositionFromString(string positionString) {
+		Vector2 result = new Vector2 ();
+		result.x = int.Parse(Regex.Match(positionString, @"^\d+").Value);
+		result.y = int.Parse(Regex.Match(positionString, @"\d+$").Value);
+		return result;
+	}
+
+}
---
+++ Assets/Zweetlogic/Scripts/GridItem.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GridItem : MonoBehaviour {
+
+	[HideInInspector] public Vector2 position;
+
+	private GridItemGraphic itemGraphic;
+	public bool used = true;
+	public GameLogic.ItemStates state = GameLogic.ItemStates.Off;
+
+	public void setUsed(bool used) {
+		this.used = used;
+		this.gameObject.SetActive (used);
+	}
+
+	public void changeToType(GameObject gameObject, bool animated, Vector3 randomOffset) {
+		
+		this.itemGraphic = null;
+
+		List<GameObject> children = new List<GameObject>();
+		foreach (Transform child in transform) children.Add(child.gameObject);
+		children.ForEach(child => Destroy(child));
+
+		GameObject newChild = GameObject.Instantiate (gameObject);
+		newChild.transform.position = randomOffset;
+		newChild.transform.SetParent (transform, false);
+
+		// Sets the BoxCollider's center to the offset of the child.
+		// If this is changed to a SphereCollider, we have to update this.
+		var collider = this.GetComponent<BoxCollider> ();
+		collider.center = randomOffset;
+
+		GridItemGraphic itemGraphic = newChild.GetComponent<GridItemGraphic> ();
+		this.itemGraphic = itemGraphic;
+	}
+
+	public void changeToState(GameLogic.ItemStates state, bool animated) {
+		this.state = state;
+		if (this.itemGraphic) {
+			this.itemGraphic.changeToState (state, animated);
+		}
+	}
+
+	public void OnMouseDown() {
+		// Helper function to help development. Todo: remove before release.
+		this.toggleState ();
+	}
+
+	public void makeHighlighted() {
+		if (this.itemGraphic) {
+			this.itemGraphic.makeHighlighted ();
+		}
+	}
+	public void makeUnhighlighted() {
+		if (this.itemGraphic) {
+			this.itemGraphic.makeUnhighlighted ();
+		}
+	}
+
+	public void toggleState() {
+		GameLogic.ItemStates newState;
+		if (this.state == GameLogic.ItemStates.Off) {
+			newState = GameLogic.ItemStates.On;
+		} else {
+			newState = GameLogic.ItemStates.Off;
+		}
+
+		this.changeToState(newState, true);
+	}
+}
