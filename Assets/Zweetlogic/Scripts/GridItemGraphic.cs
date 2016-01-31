9641903cb98a26dcdbd5a5fcd11d3d56151af7cf
Robert Mooijman <jambi@Roberts-MacBook-Pro.local>
2016-01-31 12:52:41 +0100

    lodge2

--- Assets/Zweetlogic/Scripts/GridItemGraphic.cs
+++ Assets/Zweetlogic/Scripts/GridItemGraphic.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class GridItemGraphic : MonoBehaviour {
 
@@ -9,6 +10,17 @@ public class GridItemGraphic : MonoBehaviour {
 	public string type = "SET THIS";
 	public GameObject spriteOn;
 	public GameObject spriteOff;
+
+	private AudioSource source;
+
+	public AudioClip clipOn;
+	public AudioClip clipOff;
+
+	private Vector3 baseScale;
+
+	public bool isActive(){
+		return allowInteraction;
+	}
 
 	public void makeHighlighted() {
 		highlightModel.SetActive (true);
@@ -30,14 +42,59 @@ public class GridItemGraphic : MonoBehaviour {
 		}
 	}
 
+	void Start(){
+		source = GetComponent<AudioSource> ();
+		baseScale = transform.localScale;
+	}
 
 	public void Show(){
 
-		//do random popup animation
+		transform.localScale = Vector3.zero;
+		float d = Random.value;
+		transform.DOScale (baseScale, 0.5f).SetEase (Ease.OutBack).SetDelay (d).OnComplete(Activate);
+		transform.DOShakeRotation(0.5f, 10f, 8, 50).SetDelay(d);
+		if (clipOn != null) {
+			source.clip = clipOn;
+			source.Play ();
+		}
 	}
 
-	public void Hide(){
-	
-		//
+	private void Activate(){
+		allowInteraction = true;
+	}
+
+	bool allowInteraction = false;
+
+	public void Hide(bool isPoint){
+		allowInteraction = false;
+
+		if (isPoint) {
+			//stay and float into the fire
+
+		} else {
+			//hide
+			transform.DOScale(Vector3.zero, 0.4f).SetEase(Ease.InExpo).SetDelay(Random.value * 0.5f);
+		}
+
+
+		if (clipOff != null) {
+			source.clip = clipOff;
+			source.Play ();
+		}
+	}
+
+	public void RollOver(){
+		if (!allowInteraction)
+			return;
+		//scale up
+		transform.DOScale(baseScale * 1.2f, 0.4f).SetEase(Ease.OutBack);
+
+	}
+
+	public void RollOut(){
+		if (!allowInteraction)
+			return;
+		//scale down
+		transform.DOScale(baseScale, 0.8f).SetEase(Ease.OutExpo);
 	}
 }
